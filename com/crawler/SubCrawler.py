7e3f371dc0fea201f625ca7285b2f717e1b47ef7
Fabien Lamarque <fabien.lamarque+git@gmail.com>
2016-01-14 14:20:11 +0100

    Génération de fichiers JSON

--- com/crawler/SubCrawler.py
+++ com/crawler/SubCrawler.py
@@ -14,26 +14,40 @@ def extract_submissions():
     for submission in hot_topics:
         if submission.author.name == "LoLeventVoDs":
             print(submission.title)
-            #     print()
-            # print(submission.selftext.lower())
+            lines = split_text(submission.selftext)
+            for line in lines:
+                if has_video_url(line):
+                    print(line)
+                    generate_data_structure(line, submission.title)
 
 
 def has_video_url(string):
-    return re.search("(www\.youtube\.com)|(www\.twitch\.tv)", string)
+    return re.search("(www\.youtube\.com).*(www\.twitch\.tv)", string)
 
 
 def split_text(string):
     return string.split("\n")
 
 
-def generate_data_structure(param):
+def generate_data_structure(param, title):
     data = {'full_text': param}
     tokens = param.split("|")
+    data['title'] = title
     data['local_id'] = tokens[0]
     data['blue_team'] = tokens[1]
     data['red_team'] = tokens[3]
     data['pick_ban'] = {'twitch': tokens[4], 'youtube': tokens[6]}
+    data['game_start'] = {'twitch': tokens[5], 'youtube': tokens[7]}
+    data['highlights'] = tokens[8]
+
     return data
+
+
+def write_json_file(data_struct):
+    name_file = data_struct.title + '_' + data_struct['local_id']
+    with open('com/resources/' + name_file, 'w') as f:
+        f.write(to_json(data_struct))
+        f.close()
 
 
 def to_json(data):
--- com/tests/SubCrawlerTestCase.py
+++ com/tests/SubCrawlerTestCase.py
@@ -1,17 +1,15 @@
 import unittest
-
 from com.crawler.SubCrawler import has_video_url, generate_data_structure, split_text
 
 
 class SubCrawlerTestCase(unittest.TestCase):
     def test_presence_youtube_video_in_itself(self):
-        self.assertTrue(has_video_url("https://www.youtube.com/watch?v=mlzrhbpmyai"))
-
-    def test_presence_another_video(self):
-        self.assertTrue(has_video_url("https://www.youtube.com/watch?v=ltksvh05huy"))
+        self.assertTrue(has_video_url(
+            "https://www.youtube.com/watch?v=mlzrhbpmyai, http://www.twitch.tv/esl_lol/v/30574431?t=5h09m40s  "))
 
     def test_presence_twitch_video(self):
-        self.assertTrue(has_video_url("http://www.twitch.tv/esl_lol/v/30574431?t=5h09m40s   "))
+        self.assertTrue(has_video_url(
+            "http://www.twitch.tv/esl_lol/v/30574431?t=5h09m40s , https://www.youtube.com/watch?v=mlzrhbpmyai  "))
 
     def test_split_text(self):
         self.assertEquals(len(split_text("* **format:**\n" +
@@ -25,7 +23,7 @@ class SubCrawlerTestCase(unittest.TestCase):
             ' | [picks &amp; bans](https://www.youtube.com/watch?v=ytsjecr73nk)'
             ' | [game start](https://www.youtube.com/watch?v=ytsjecr73nk&t=7m45s)'
             ' | [highlights](https://www.youtube.com/watch?v=j67z3z9gnsc)'
-            ' |')
+            ' |', 'title')
         self.assertIsNotNone(data_struct)
 
     def test_generate_teams(self):
@@ -38,8 +36,8 @@ class SubCrawlerTestCase(unittest.TestCase):
             ' [game start](http://www.twitch.tv/esl_lol/v/30574431?t=1h57m05s) |'
             ' [picks &amp; bans](https://www.youtube.com/watch?v=ytsjecr73nk) |'
             ' [game start](https://www.youtube.com/watch?v=ytsjecr73nk&t=7m45s) |'
-            ' [highlights](https://www.youtube.com/watch?v=j67z3z9gnsc) |'
-        )
+            ' [highlights](https://www.youtube.com/watch?v=j67z3z9gnsc) |',
+            'title')
         self.assertTrue('qg' in data_struct['blue_team'])
         self.assertTrue('dig' in data_struct['red_team'])
         self.assertTrue('a2' in data_struct['local_id'])
@@ -47,6 +45,10 @@ class SubCrawlerTestCase(unittest.TestCase):
         self.assertIsNotNone(data_struct['pick_ban']['twitch'])
         self.assertIsNotNone(data_struct['pick_ban']['youtube'])
         self.assertIsNotNone(data_struct['game_start'])
+        self.assertIsNotNone(data_struct['game_start']['youtube'])
+        self.assertEqual(data_struct['game_start']['youtube'],
+                         ' [game start](https://www.youtube.com/watch?v=ytsjecr73nk&t=7m45s) ')
+        self.assertIsNotNone(data_struct['highlights'])
 
         if __name__ == '__main__':
             unittest.main()
